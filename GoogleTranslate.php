a1ac70c180b96bcb680d7af848de86a8c0b9d584
Emanuele Corradini <korra88@gmail.com>
2015-05-04 12:59:08 +0200

    Varius fix and improvement.

--- GoogleTranslate.php
+++ GoogleTranslate.php
@@ -2,11 +2,12 @@
 
 /**
  * Google-Translate-API
- * New API Library for Google translate V2 in PHP 
- * @link https://github.com/viniciusgava/Google-Translate-API
- * @license http://www.gnu.org/copyleft/gpl.html
- * @version 1.0
+ * New API Library for Google translate V2 in PHP
+ * @link https:// Github.com/korra88/Google-Translate-API
+ * @license http:// Www.gnu.org/copyleft/gpl.html
+ * @version 1.1
  * @author Vinicius Gava (gava.vinicius@gmail.com)
+ * @author Emanuele Corradini (korra88@gmail.com)
  */
 class GoogleTranslate {
 
@@ -14,11 +15,11 @@ class GoogleTranslate {
      * URI API
      * @var string
      */
-    private $apiUri = 'https://www.googleapis.com/language/translate/v2';
+    private $apiUri = 'https:// Www.googleapis.com/language/translate/v2';
 
     /**
      * Access Key to API
-     * @var string 
+     * @var string
      */
     private $accessKey = '';
 
@@ -38,10 +39,12 @@ class GoogleTranslate {
      * Service translate text
      */
     CONST SERVICE_TRANSLATE = 'translate';
+
     /**
      * Service detect language
      */
     CONST SERVICE_DETECT = 'detect';
+
     /**
      * Service language support
      */
@@ -50,15 +53,16 @@ class GoogleTranslate {
     public function __construct($accessKey) {
         $this->setAccessKey($accessKey);
     }
+
     /**
      * Set access key
-     * @param string $key 
+     * @param string $key
      */
     public function setAccessKey($key) {
         if (strlen($key) == 39) {
             $this->accessKey = $key;
         } else {
-            throw new GoogleTranslateInvalidKey();
+            throw new GoogleTranslateException("Invalid Access Key");
         }
     }
 
@@ -67,56 +71,57 @@ class GoogleTranslate {
      * @param string|array $text The text to be translated
      * @param string $targetLanguage The language to translate the source text into
      * @param string|null|array $sourceLanguage The language of the source text. If a language is not specified, the system will attempt to identify the source language automatically
+     * @return text|array|false
      */
     public function translate($text, $targetLanguage, &$sourceLanguage = null) {
         if ($this->isValid($text, $targetLanguage, $sourceLanguage)) {
             reset($text);
-            //add keyAccess
+            // Add keyAccess
             $this->addQueryParam('key', $this->accessKey);
-            //add text to be translate
+            // Add text to be translate
             $this->addQueryParam('q', $text);
-            //add target language
+            // Add target language
             $this->addQueryParam('target', $targetLanguage);
-            //if source not null, add param to query
-            if(!is_null($sourceLanguage)){
-                $this->addQueryParam('source', $sourceLanguage);                
+            // If source not null, add param to query
+            if (!is_null($sourceLanguage)) {
+                $this->addQueryParam('source', $sourceLanguage);
             }
-            //init connect
+            // Init connect
             $this->initConnect();
-            //get content
+            // Get content
             $result = $this->execConnect();
-            //close connect
+            // Close connect
             $this->closeConnect();
-            //verify this is multiple text
+            // Verify this is multiple text
             if (!is_array($text)) {
-                //get only info necessary 
+                // Get only info necessary
                 $result = current($result->translations);
-                
+
                 if (isset($result->detectedSourceLanguage)) {
-                    //return by reference the language in case detected language
+                    // Return by reference the language in case detected language
                     $sourceLanguage = $result->detectedSourceLanguage;
                 }
-                
-                //return translate
+
+                // Return translate
                 return $result->translatedText;
             } else {
-                //this is multiple text
-                //get only info necessary
+                // This is multiple text
+                // Get only info necessary
                 $result = $result->translations;
-                //save translate list
+                // Save translate list
                 $arrTranslateReturn = array();
-                //save source list
+                // Save source list
                 $arrSourceReturn = array();
-                //get translates
+                // Get translates
                 foreach ($result as $itemResult) {
                     $arrTranslateReturn[] = $itemResult->translatedText;
                     if (isset($itemResult->detectedSourceLanguage)) {
                         $arrSourceReturn[] = $itemResult->detectedSourceLanguage;
                     }
                 }
-                //return by reference the language in case detected language
+                // Return by reference the language in case detected language
                 $sourceLanguage = $arrSourceReturn;
-                //return list of translate
+                // Return list of translate
                 return $arrTranslateReturn;
             }
         } else {
@@ -128,47 +133,47 @@ class GoogleTranslate {
      * Deletect Language
      * @param string|array $text The text or list the text to be detect
      * @param null|string|array $isReliable is reliable
-     * @return string|array language or list of language detected 
+     * @return string|array language or list of language detected
      */
     public function detect($text, &$isReliable = null) {
         if ($this->isValid($text, null, null, false)) {
             reset($text);
 
-            //add keyAccess
+            // Add keyAccess
             $this->addQueryParam('key', $this->accessKey);
-            //add text to be translate
+            // Add text to be translate
             $this->addQueryParam('q', $text);
-            //init connect
+            // Init connect
             $this->initConnect(self::SERVICE_DETECT);
-            //get content
+            // Get content
             $result = $this->execConnect();
-            //close connect
+            // Close connect
             $this->closeConnect();
-            //verify this is multiple text
+            // Verify this is multiple text
             if (count($result->detections) == 1) {
-                //get only info necessary 
+                // Get only info necessary
                 $result = current(current($result->detections));
-                //return by reference the language is realiable
+                // Return by reference the language is realiable
                 $isReliable = $result->isReliable;
-                //return translate
+                // Return translate
                 return $result->language;
             } else {
-                //this is multiple text
-                //get only info necessary
+                // This is multiple text
+                // Get only info necessary
                 $result = $result->detections;
-                //save is reliable list
+                // Save is reliable list
                 $arrIsReliable = array();
-                //save detect language list
+                // Save detect language list
                 $arrSourceReturn = array();
-                //get translates
+                // Get translates
                 foreach ($result as $itemResult) {
                     $itemResult = current($itemResult);
                     $arrIsReliable[] = $itemResult->isReliable;
                     $arrSourceReturn[] = $itemResult->language;
                 }
-                //return by reference the language is realiable
+                // Return by reference the language is realiable
                 $isReliable = $arrIsReliable;
-                //return list of detect language
+                // Return list of detect language
                 return $arrSourceReturn;
             }
         } else {
@@ -177,82 +182,84 @@ class GoogleTranslate {
     }
 
     /**
-     * Language support
-     * @param string|null $target language target
+     * Request Language support list
+     * @param type $target
+     * @return array|boolean
      */
     public function languageSupport($target = null) {
-        //add keyAccess
+        // Add keyAccess
         $this->addQueryParam('key', $this->accessKey);
-        if(!is_null($target)){
+        if (!is_null($target)) {
             if ($this->validLanguage($target)) {
-                $this->addQueryParam('target', $target);                
-            }else{
+                $this->addQueryParam('target', $target);
+            } else {
                 return false;
-            }            
+            }
         }
-        //init connect
+        // Init connect
         $this->initConnect(self::SERVICE_LANGUAGE);
-        //get content
+        // Get content
         $result = $this->execConnect();
-        //close connect
+        // Close connect
         $this->closeConnect();
-        
-        //get only info necessary
-        $result = $result->languages;        
-        //return list of language support
+
+        // Get only info necessary
+        $result = $result->languages;
+        // Return list of language support
         return $result;
-        
     }
 
     /**
-     * Validate info 
+     * Validate info
      * @param string|array $text The text or list the text to be validate
      * @param string $targetLanguage target language to be validate
      * @param string $sourceLanguage source language to be validate
      * @param boolean $targetRequired the target language is required?
-     * @return boolean 
+     * @return boolean
      */
     private function isValid(&$text, $targetLanguage = null, $sourceLanguage = null, $targetRequired = true) {
-        //in case of numeric only in text, return false
+        // In case of numeric only in text, return false
         if (!is_array($text)) {
             $text = array($text);
         }
-        //valid list of translate
+        // Valid list of translate
         foreach ($text as $keyText => $oneText) {
-            //is numeric?
+            // Is numeric?
             if (is_numeric($oneText) || strlen($oneText) < 2) {
-                //remove from list translate
+                // Remove from list translate
                 unset($text[$keyText]);
             }
         }
-        //no text valid?
+        // No text valid?
         if (count($text) <= 0) {
             return false;
         }
-        //target required?
+        // Target required?
         if ($targetRequired) {
-            //is valid language target
+            // Is valid language target?
             if (!$this->validLanguage($targetLanguage)) {
                 return false;
             }
         }
-        //is valid language source
+        // Is valid language source?
         if (!is_null($sourceLanguage)) {
             if (!$this->validLanguage($sourceLanguage)) {
                 return false;
             }
         }
-        //valid!
+        // Valid!
         return true;
     }
 
     /**
-     * validate language
+     * Validate language with a regular expression:<br/>
+     * 2 characters + hypens + 2 characters ( <b>%([a-z]{2})(-[A-Z]{2})?%</b> ).<br/>
+     * Real supported languages list available at https://cloud.google.com/translate/v2/using_rest#language-params<br/>
      * @param string $lang language text to be validate
-     * @return boolean 
+     * @return boolean
      */
-    private function validLanguage($lang) {
-        $regexpValidLanguage = '%([a-z]{2})(-[a-z]{2})?%';
+    public function validLanguage($lang) {
+        $regexpValidLanguage = '%([a-z]{2})(-[A-Z]{2})?%';
         if (preg_match($regexpValidLanguage, $lang) == 0) {
             return false;
         } else {
@@ -261,45 +268,80 @@ class GoogleTranslate {
     }
 
     /**
-     * Init the connect with parameters find
+     * Init the connect with parameters set in <b>$this->parameters</b>.<br/>
+     * Connection set in <b>$this->connect</b>.
+     * @param string $service           One of the values of GoogleTranslate's SERVICE_DETECT, SERVICE_LANGUAGE or SERVICE_TRANSLATE contants. Defaults to SERVICE_TRANSLATE.
+     * @throws GoogleTranslateException
      */
     private function initConnect($service = self::SERVICE_TRANSLATE) {
-        //choose service
-        switch ($service) {
-            case self::SERVICE_DETECT : $url = $this->apiUri . '/detect';
-                break;
-            case self::SERVICE_LANGUAGE : $url = $this->apiUri . '/languages';
-                break;
-            case self::SERVICE_TRANSLATE : default: $url = $this->apiUri;
+        // Without API key nothing can be done!
+        if (!isset($this->parameters['key'])) {
+            throw new GoogleTranslateException('Missing API key ("key").');
         }
-        //case exists parameters, add to url
-        if (count($this->parameters) > 0) {
-            $url .= '?';
-            //add for each item 
-            foreach ($this->parameters as $keyParam => $param) {
-                //used in case de multiple text translate
-                if (is_array($param)) {
-                    foreach ($param as $subParam) {
-                        $url .= '&' . urlencode($keyParam) . '=' . urlencode($subParam);
-                    }
-                } else {
-                    $url .= '&' . urlencode($keyParam) . '=' . urlencode($param);
+        // Choose service
+        switch ($service) {
+            case self::SERVICE_DETECT:
+                $url = $this->apiUri . '/detect';
+                if (!isset($this->parameters['q'])) {
+                    throw new GoogleTranslateException('Missing one or more required parameters ("q").');
                 }
+                break;
+            case self::SERVICE_LANGUAGE:
+                $url = $this->apiUri . '/languages';
+                break;
+            case self::SERVICE_TRANSLATE:
+            default:
+                $url = $this->apiUri;
+                // Check mandatory parameters
+                if (!isset($this->parameters['q']) || !isset($this->parameters['target'])) {
+                    throw new GoogleTranslateException('Missing one or more required parameters ("q", "target").');
+                }
+        }
+
+        // Text must be smaller than 5K chars
+        if (isset($this->parameters['q'])) {
+            $text_len = strlen($this->parameters['q']);
+            if ($text_len > 5000) {
+                throw new GoogleTranslateException("Can't translate more than 5K chars.");
             }
         }
-        //init curl
-        $this->connect = curl_init($url);
-        //return data receive
+
+        // If GET url is longer than 2K char, request must be POST
+        if (!empty($this->parameters)) {
+            $query = http_build_query($this->parameters);
+            if (strlen($url . '?' . $query) < 2000) {
+                // Init curl as GET request with http query parameters
+                $url .= '?' . $query;
+                $this->connect = curl_init($url);
+            } else {
+                // Init curl as POST request
+                $this->connect = curl_init($url);
+                /**
+                 * If $this->parameters is multidimensional count($this->prameters) won't be correct
+                 * so we count the exploded query string
+                 */
+                curl_setopt($this->connect, CURLOPT_POST, count(explode('&', $query)));
+                curl_setopt($this->connect, CURLOPT_POSTFIELDS, $query);
+                curl_setopt($this->connect, CURLOPT_HTTPHEADER, array(
+                    'X-HTTP-Method-Override: GET'
+                ));
+            }
+        } else {
+            // Init curl as empty GET request
+            $this->connect = curl_init($url);
+        }
+
+        // Set to return data received
         curl_setopt($this->connect, CURLOPT_RETURNTRANSFER, true);
     }
 
     /**
-     * Add Query param in connect
+     * Add Query parameters for the API request (on $this->paramters)
      * @param type $key
-     * @param type $value 
+     * @param type $value
      */
     private function addQueryParam($key, $value) {
-        //remove possible whitespaces, utf8 encode AND add to params list
+        // Remove possible whitespaces, utf8 encode AND add to params list
         if (is_array($value)) {
             foreach ($value as $keyValue => $itemValue) {
                 $value[$keyValue] = utf8_encode($itemValue);
@@ -307,65 +349,54 @@ class GoogleTranslate {
         } else {
             $value = utf8_encode($value);
         }
-        //add to param list
+        // Add to param list
         $this->parameters[utf8_encode(str_replace(' ', '', $key))] = $value;
     }
 
     /**
-     * Close the connect
+     * Close the CURL request ($this->connect) and reset parameters.
      */
     private function closeConnect() {
-        //close curl connect
+        // Close curl connect
         curl_close($this->connect);
-        //clear params to next request
+        // Clear params to next request
         $this->parameters = array();
     }
+
     /**
-     * Execute connect and return array with data
-     * @return array  
+     * Execute CURL $this->connect and return array with data (already JSON decoded)
+     * @return array
+     * @throws GoogleTranslateException
      */
     private function execConnect() {
-        //exec curl
+        // Exec curl
         $result = curl_exec($this->connect);
-        //transform json in stdClass
+        // Transform json in stdClass
         $result = json_decode($result);
 
-        //get request info
+        // Get request info
         $arrInfo = curl_getinfo($this->connect);
-        //found?
+        // Found?
         if ($arrInfo['http_code'] == 404) {
-            //no connect
-            throw new GoogleTranslateNotFoundException();
+            // No connect
+            throw new GoogleTranslateException('Not Found Request', 404);
         }
 
         if (($arrInfo['http_code'] == 200 || $arrInfo['http_code'] == 304) && !isset($result->error)) {
-            //request ok, return data
+            // Request ok, return data
             return $result->data;
         } else {
-            //invalid key
-            throw new GoogleTranslateInvalidKeyException();
+            // Invalid key
+            throw new GoogleTranslateException("Invalid Access Key");
         }
     }
 
 }
 
-/**
- * Google Translate Exception Invalid Access Key
- */
-class GoogleTranslateInvalidKeyException extends Exception {
+class GoogleTranslateException extends Exception {
 
-    function __construct() {
-        parent::__construct('Invalid Access Key');
+    function __construct($message, $code = null, $previus = null) {
+        parent::__construct($message, $code, $previus);
     }
 
-}
-
-/**
- * Google Translate Exception Not found 404, probable problem in connect internert
- */
-class GoogleTranslateNotFoundException extends Exception {
-    
-    function __construct() {
-        parent::__construct('Not Found Request', 404);
-    }
 }
---
+++ README.md
@@ -0,0 +1,36 @@
+### Google Translate library
+
+Improvement made to (https://github.com/viniciusgava/Google-Translate-API)[viniciusgava's Google-Translate-API] library.
+
+Please note that to use Google Translate APIs you have to activate billing in your project and use an API key (these are paid APIs!).
+
+#### Usage
+
+Translation:
+
+```
+require_once '/path/to/GoogleTranslate.php';
+
+try {
+    $GT = new GoogleTranslate('YOUR_VALID_API_KEY');
+
+    // Galli Caesaris saevitia @ http://www.thelatinlibrary.com/ammianus/14.shtml
+    $text = 'Post emensos insuperabilis expeditionis eventus languentibus partium animis, quas periculorum varietas fregerat et laborum, nondum tubarum cessante clangore vel milite locato per stationes hibernas, fortunae saevientis procellae tempestates alias rebus infudere communibus per multa illa et dira facinora Caesaris Galli, qui ex squalore imo miseriarum in aetatis adultae primitiis ad principale culmen insperato saltu provectus ultra terminos potestatis delatae procurrens asperitate nimia cuncta foedabat. Propinquitate enim regiae stirpis gentilitateque etiam tum Constantini nominis efferebatur in fastus, si plus valuisset, ausurus hostilia in auctorem suae felicitatis, ut videbatur.';
+    
+    // Translate latin to english
+    $translated_text = $GT->translate($text, 'en', 'la');
+    
+    echo "Galli Caesaris saevitia<br/>" . $translated_text;
+
+} catch (GoogleTranslateException $e) {
+    echo "Error: " . $e->message;
+}
+
+```
+
+#### Notes
+
+The library doesn't check if the language is supported but that the language code is well formatted, for the complete list of supported languages
+visits (https://cloud.google.com/translate/v2/using_rest#language-params)[Google Translate API docs].
+
+Correctly switches to POST request when query url is longer than 2K characters.
